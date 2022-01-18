0be5956d31cdab9843041b0b2fd29d5ad7f292d6
RAZAFINDRANAIVO <83865592+RAZAFINDRANAIVO@users.noreply.github.com>
2022-01-18 12:10:33 +0100

    first commit

---
+++ app/Controllers/Chat.php
@@ -0,0 +1,18 @@
+<?php 
+
+namespace App\Controllers;
+
+class Chat extends BaseController
+{
+	public function index()
+	{
+		$data = [];
+
+		echo view('templates/header', $data);
+		echo view('chat');
+		echo view('templates/footer');
+	}
+
+	//--------------------------------------------------------------------
+
+}
